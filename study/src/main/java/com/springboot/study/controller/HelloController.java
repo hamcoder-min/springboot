e7f9a966af4716ad412ba02e6daf1ffe787f52a7
Kim <a01064149895@gmail.com>
2025-10-13 16:21:18 +0900

    2025-10-13 Spring commit!

--- study/src/main/java/com/springboot/study/controller/HelloController.java
+++ study/src/main/java/com/springboot/study/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.springboot.study.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,9 +11,16 @@ public class HelloController {
      * 예) /hello
      */
     @GetMapping("/hello")
-    public String hello() {
-        System.out.println("-----> HelloController!!!");
+    public String hello(Model model) {
+        model.addAttribute("pathName", "hello");
+        model.addAttribute("data", "홍길동 친구");
         return "hello";     //view name
     }
 
+    @GetMapping("/hello2")
+    public String hello2(Model model) {
+        model.addAttribute("pathName", "hello2");
+        model.addAttribute("data", "홍길동");
+        return "hello";
+    }
 }
